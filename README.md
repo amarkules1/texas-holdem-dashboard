7c417873d55a8ca390cc0820e72dddcedd633670
Alex Markules <amarlar2354@gmail.com>
2024-06-10 17:31:33 -0400

    working hole-card dashboard

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # texas-holdem-dashboard
+
+run app: `pipenv run flask --app main:app run`
+
+regen requirements.txt: `pipenv lock -r > requirements.txt`
---
+++ texas-holdem-frontend/src/components/HelloWorld.vue
@@ -0,0 +1,126 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h2>First Card</h2>
+    <div class="cardSelector">
+      <div v-for="suit in suits" :key="suit" class="suitRow">
+        <div v-for="rank in ranks" :key="rank" :class="{card: true, selected: firstRank == rank && firstSuit == suit}" @click="selectFirstCard(rank, suit)">
+          <img class="cardImage" :src="require(`../assets/cards/${suit}_${rank}.svg`)" alt="card" />
+        </div>
+      </div>
+    </div>
+    <h2>Second Card</h2>
+    <div class="cardSelector">
+      <div v-for="suit in suits" :key="suit" class="suitRow">
+        <div v-for="rank in ranks" :key="rank" :class="{card: true, selected: secondRank == rank && secondSuit == suit}" @click="selectSecondCard(rank, suit)">
+          <img class="cardImage" :src="require(`../assets/cards/${suit}_${rank}.svg`)" alt="card" />
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="stats">
+    <h3>Hand Performance</h3>
+    <ul>
+      <li>
+        Card #1: {{ perf.card_1 }}
+      </li>
+      <li>
+        Card #2: {{ perf.card_2 }}
+      </li>
+      <li>
+        Win Rate: {{ perf.win_rate }}
+      </li>
+      <li>
+        Rank: {{ perf.rank }} of 2652
+      </li>
+      <li>
+        Percentile: {{ perf.percentile }}%
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  name: 'HelloWorld',
+  data() {
+    return {
+      firstRank: "",
+      firstSuit: "",
+      secondRank: "",
+      secondSuit: "",
+      ranks: ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'jack', 'queen', 'king', 'ace'],
+      suits: ['clubs', 'diamonds', 'hearts', 'spades'],
+      perf: {}
+    }
+  },
+  methods: {
+    selectFirstCard(rank, suit) {
+      this.firstRank = rank;
+      this.firstSuit = suit;
+      if (this.secondRank && this.secondSuit) {
+        this.getHandPerformance()
+      }
+    },
+    selectSecondCard(rank, suit) {
+      this.secondRank = rank;
+      this.secondSuit = suit;
+      if (this.firstRank && this.firstSuit) {
+        this.getHandPerformance()
+      }
+    },
+    async getHandPerformance() {
+      let firstCardRank = this.firstRank
+      let secondCardRank = this.secondRank
+      if (firstCardRank.length > 2) {
+        firstCardRank = firstCardRank[0].toUpperCase()
+      }
+      if (secondCardRank.length > 2) {
+        secondCardRank = secondCardRank[0].toUpperCase()
+      }
+      firstCardRank += ` of ${this.firstSuit.charAt(0).toUpperCase() + this.firstSuit.slice(1)}`
+      secondCardRank += ` of ${this.secondSuit.charAt(0).toUpperCase() + this.secondSuit.slice(1)}`
+
+      const response = await axios.get(`http://localhost:5000/card-stats?card1=${firstCardRank}&card2=${secondCardRank}`)
+      this.perf = response.data
+    }
+  },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+
+.card {
+  display: inline-block;
+  margin: 5px 10px;
+  width: 4%;
+  border: 1px solid black;
+}
+
+.selected {
+  border: 2px solid red;
+}
+
+.cardImage {
+  width: 100%;
+}</style>

44b954403b8304d1d90761fe14db8577f9907af0
amarkules1 <amarlar2354@gmail.com>
2025-02-15 11:12:53 -0500

    fix docker run command

--- start_prod.sh
+++ start_prod.sh
@@ -4,7 +4,7 @@ docker stop ${app}
 docker rm ${app}
 docker rmi ${app}
 docker build -t ${app} -f Dockerfile.prod .
-docker run -d -p 5003:5003 \
+docker run -d -p 443:443 \
   --name=${app} \
   -v $PWD:/app ${app}
 read -n1 -s

d7697b3d9ba1b80a9d6b754e2656408f6c03b81c
amarkules1 <amarlar2354@gmail.com>
2025-02-15 10:33:40 -0500

    productionize it

---
+++ start_prod.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+app="texasholdem"
+docker stop ${app}
+docker rm ${app}
+docker rmi ${app}
+docker build -f Dockerfile.prod-t ${app} .
+docker run -d -p 5003:5003 \
+  --name=${app} \
+  -v $PWD:/app ${app}
+read -n1 -s

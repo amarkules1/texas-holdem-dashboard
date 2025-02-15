734fc624f4d23dc758e8d3a33ef1744402f8b220
amarkules1 <amarlar2354@gmail.com>
2025-02-15 11:07:14 -0500

    fix start_prod.sh

--- start_prod.sh
+++ start_prod.sh
@@ -3,7 +3,7 @@ app="texasholdem"
 docker stop ${app}
 docker rm ${app}
 docker rmi ${app}
-docker build -f Dockerfile.prod-t ${app} .
+docker build -t ${app} -f Dockerfile.prod .
 docker run -d -p 5003:5003 \
   --name=${app} \
   -v $PWD:/app ${app}
